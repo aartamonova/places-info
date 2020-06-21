7666182c5b2a266e15401674d0d12dee6a684ed1
aartamonova <artamonova.a@yahoo.com>
2020-06-21 14:16:22 +0300

    worker returned

--- places_info/places_info_utils.py
+++ places_info/places_info_utils.py
@@ -309,7 +309,7 @@ def get_statistic_helper(login, action_type):
     return response_500_error()
 
 
-def send_statistic_helper(action, result, name=None):
+def send_statistic(action, result, name=None):
     '''Отправить действие пользователя в статистику'''
     data = form_action_data(action, result, name)
     if data:
@@ -319,7 +319,7 @@ def send_statistic_helper(action, result, name=None):
             pass
 
 
-def send_statistic(action, result, name=None):
+def send_statistic_helper(action, result, name=None):
     '''Отправить задачу в очередь'''
     queue.enqueue(send_statistic, action, result, name)
 
